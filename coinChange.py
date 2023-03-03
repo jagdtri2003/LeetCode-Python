e4ecc4d185e85537b2e8224e533039905cb93930
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-03-03 11:49:49 +0530

    new file:   coinChange.py

---
+++ coinChange.py
@@ -0,0 +1,26 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        def solve(idx,target,dp):
+            if idx==0:
+                if target%coins[0]==0: #Check krenge agr divide hoga tb required return krdenge wrna ik bdi value
+                    return target/coins[0]
+                return 1e9
+            
+            if dp.get((idx,target)):            #Using DP
+                return dp.get((idx,target))
+
+            not_take=0+solve(idx-1,target,dp)   #Not Taking the denomination , so idx-1 ho jayega but target will be same 
+            # 
+            take=float('inf')
+
+            if coins[idx]<=target:
+                take=1+solve(idx,target-coins[idx],dp)
+
+            dp[(idx,target)]=min(not_take,take)
+            
+            return dp[(idx,target)]
+        
+        ans=solve(len(coins)-1,amount,{})
+        if ans!=1e9:
+            return int(ans)
+        return -1
