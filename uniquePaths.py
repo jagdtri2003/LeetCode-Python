4d52a4f086101b73bb45f5d6c672f43d14f14747
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-03-04 10:00:37 +0530

    modified:   uniquePaths.py

--- uniquePaths.py
+++ uniquePaths.py
@@ -5,13 +5,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         def solve(row,col,dp):
-            if row==m-1 or col==n-1:
+            if row==m-1 or col==n-1: #Agar Last Row Ya Column me honge toh ik hi rasta hoga bss 
                 return 1
                 
             if dp.get((row,col)):
                 return dp[(row,col)]
 
-            dp[(row,col+1)]=solve(row,col+1,dp)
+            dp[(row,col+1)]=solve(row,col+1,dp)         #Using DP to Store
             dp[(row+1,col)]=solve(row+1,col,dp)
         
             return dp[(row,col+1)] + dp[(row+1,col)]
