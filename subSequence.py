a70112944bb439b0e5ef70f4a049cf1b0f301c06
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-03-05 13:32:20 +0530

    modified:   subSequence.py

--- subSequence.py
+++ subSequence.py
@@ -1,4 +1,5 @@
-def subSequence(arr,n):
+def subSequence(arr):
+    n=len(arr)
     ans=[]
     def rec(idx,lst):
         if idx>=n:
@@ -9,6 +10,23 @@ def subSequence(arr,n):
     rec(0,[])
     return ans
 
-arr1=[3,2,1]
-a=subSequence(arr1,3)
-print(a)
+def subSequenceWithSum(arr,sum):
+    n=len(arr)
+    ans=[]
+    def dfs(idx,lst,total):
+        if idx>=n:
+            if total==sum:
+                ans.append(lst)
+            return 
+        dfs(idx+1,lst,total)
+        dfs(idx+1,lst+[arr[idx]],total+arr[idx])
+    
+    dfs(0,[],0)
+    return ans
+
+
+arr1=[3,2,1,1,2,1]
+a=subSequence(arr1)
+b=subSequenceWithSum(arr1,3)
+print(b)
+# print(a)
