fa3743c446ade03b1d0c4eee2e3eea3c7e210bf9
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-03-06 22:21:09 +0530

    new file:   nQueens.py

---
+++ subs.py
@@ -0,0 +1,15 @@
+def sumDiff(S, n):
+    #Code here
+    ans=0
+    def solve(idx,lst):
+        nonlocal ans
+        if idx>=n:
+            if len(lst)>0:
+                ans+=lst[-1]-lst[0]
+            return 
+        solve(idx+1,lst)
+        solve(idx+1,lst+[S[idx]])
+    solve(0,[])
+    return ans
+
+print(sumDiff([5,8,7],3))
