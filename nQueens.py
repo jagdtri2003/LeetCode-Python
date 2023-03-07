f92bfd35cae0c60e539a43d3ae8303c5f69f1e60
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-03-07 22:42:13 +0530

    modified:   nQueens.py

--- nQueens.py
+++ nQueens.py
@@ -1,19 +1,74 @@
+"""
+                    BackTracking 
+"""
+
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def backtrack(row):
-            if row == n:
-                solutions.append(["".join(row) for row in board])
-                return
-            for col in range(n):
-                if not cols[col] and not diag[row+col] and not anti_diag[row-col]:
-                    board[row][col] = 'Q'
-                    cols[col], diag[row+col], anti_diag[row-col] = True, True, True
-                    backtrack(row+1)
-                    board[row][col] = '.'
-                    cols[col], diag[row+col], anti_diag[row-col] = False, False, False
+        ans=[]
+        def is_safe(row,col,board):
+            dcol=col
+            while dcol>=0:
+                if board[row][dcol]=="Q":
+                    return False
+                dcol-=1
+            dcol,drow=col,row
+            while dcol>=0 and drow>=0:
+                if board[drow][dcol]=="Q":
+                    return False
+                drow-=1
+                dcol-=1
+            dcol,drow=col,row
+            while dcol>=0 and drow<n:
+                if board[drow][dcol]=="Q":
+                    return False
+                drow+=1
+                dcol-=1
+            return True
         
-        board = [['.' for _ in range(n)] for _ in range(n)]
-        cols, diag, anti_diag = [False]*n, [False]*(2*n-1), [False]*(2*n-1)
-        solutions = []
-        backtrack(0)
-        return solutions
+        def solve(col,board):
+            if col==n:
+                ans.append(["".join(i) for i in board])
+                return 
+            for row in range(n):
+                if is_safe(row,col,board):
+                    board[row][col]="Q"
+                    solve(col+1,board)
+                    board[row][col]="."                     #BackTrack
+        board=[["." for _ in range(n)] for i in range(n)]
+        solve(0,board)
+        return ans
+
+            
+
+"""
+                        -> Optimized Solution 
+
+We don't use need to use is_safe function instead we maintain an array for left,upper left and lower left 
+
+
+"""
+
+
+
+class Solution(object):
+    def solveNQueens(self, n):
+        left,upleft,lowleft=[0]*n,[0]*(2*n-1),[0]*(2*n-1)
+        ans=[]
+        def solve(col,board):
+            if col==n:
+                ans.append(["".join(i) for i in board])
+                return 
+            for row in range(n):
+                if not left[row] and not upleft[n-1+row-col] and not lowleft[row+col]:
+                    board[row][col]="Q"
+                    left[row]=1
+                    upleft[n-1+row-col]=1
+                    lowleft[row+col]=1
+                    solve(col+1,board)
+                    board[row][col]="."
+                    left[row]=0
+                    upleft[n-1+row-col]=0
+                    lowleft[row+col]=0
+        board=[["." for i in range(n)] for _ in range(n)]
+        solve(0,board)
+        return ans
